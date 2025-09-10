1e7277f7faace1ced408faaa33b0fc9018d63aeb
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-09-10 20:04:14 +0300

    Add onboarding navigation logic and constants for splash screen

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fruiteapp/core/animation/animate_do.dart';
 import 'package:fruiteapp/core/routes/kay_route.dart';
+import 'package:fruiteapp/core/services/setup_service_locator.dart';
+import 'package:fruiteapp/core/utils/app_constants.dart';
 import 'package:fruiteapp/core/utils/app_images.dart';
 import 'package:go_router/go_router.dart';
 import 'package:svg_flutter/svg.dart';
+
+import '../../../../../core/services/shared_prefrences_services.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -20,7 +24,15 @@ class _SplashViewBodyState extends State<SplashViewBody> {
   }
 
   void excuteNavigation() {
+    final isOnboardingSeen = getIt.get<SharedPreferencesService>().getBool(
+      AppConstants.kIsOnboardingSeen,
+    );
+
     Future.delayed(Duration(seconds: 3), () {
+      if (isOnboardingSeen == true) {
+        GoRouter.of(context).pushReplacement(KayRoute.login);
+        return;
+      }
       GoRouter.of(context).pushReplacement(KayRoute.onboarding);
     });
   }
