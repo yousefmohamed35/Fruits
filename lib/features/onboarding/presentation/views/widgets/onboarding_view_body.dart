04e85ba6fc4afc8db5cc096572f8902042df37a4
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-09-10 15:16:40 +0300

    Implement OnboardingView and add OnboardingViewBody widget for onboarding screen layout

---
+++ lib/features/onboarding/presentation/views/widgets/onboarding_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnboardingViewBody extends StatelessWidget {
+  const OnboardingViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      
+    ],);
+  }
+}
