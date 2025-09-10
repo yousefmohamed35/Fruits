fb3568cd7f3a59a60689b1408df99d25a97126a2
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-09-10 14:50:00 +0300

    Add onboarding route and implement OnboardingView with navigation from SplashView and add animation to splash

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:fruiteapp/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/onboarding/presentation/views/onboarding_view.dart';
 import 'kay_route.dart';
 
 abstract class Routes {
@@ -10,6 +11,12 @@ abstract class Routes {
         path: KayRoute.splash,
         builder: (context, state) {
           return const SplashView();
+        },
+      ),
+      GoRoute(
+        path: KayRoute.onboarding,
+        builder: (context, state) {
+          return const OnboardingView();
         },
       ),
     ],
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:fruiteapp/core/animation/animate_do.dart';
+import 'package:fruiteapp/core/routes/kay_route.dart';
 import 'package:fruiteapp/core/utils/app_images.dart';
+import 'package:go_router/go_router.dart';
 import 'package:svg_flutter/svg.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    excuteNavigation();
+    super.initState();
+  }
+
+  void excuteNavigation() {
+    Future.delayed(Duration(seconds: 3), () {
+      GoRouter.of(context).pushReplacement(KayRoute.onboarding);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +32,11 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Row(children: [SvgPicture.asset(Assets.imagesPlant)]),
-        SvgPicture.asset(Assets.imagesLogoo),
+        CustomBounceInDown(
+          duration: Duration(seconds: 1),
+          child: SvgPicture.asset(Assets.imagesLogoo),
+        ),
+
         SvgPicture.asset(Assets.imagesSplashBottom, fit: BoxFit.fill),
       ],
     );
