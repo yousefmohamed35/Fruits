7886e354521c285e47066fc3671bcfa34c218924
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-09-10 18:08:53 +0300

    Refactor OnboardingViewBody to include DotsIndicator for enhanced onboarding experience

--- lib/features/onboarding/presentation/views/widgets/onboarding_view_body.dart
+++ lib/features/onboarding/presentation/views/widgets/onboarding_view_body.dart
@@ -1,5 +1,8 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:fruiteapp/features/onboarding/presentation/views/widgets/onboarding_page_view.dart';
+
+import '../../../../../core/utils/app_colors.dart';
 
 class OnboardingViewBody extends StatelessWidget {
   const OnboardingViewBody({super.key});
@@ -7,7 +10,18 @@ class OnboardingViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(children: [Expanded(child: OnboardingPageView())]),
+      child: Column(
+        children: [
+          Expanded(child: OnboardingPageView()),
+          DotsIndicator(
+            dotsCount: 2,
+            decorator: DotsDecorator(
+              activeColor: AppColors.primaryColor,
+              color: AppColors.primaryColor.withOpacity(0.3),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
