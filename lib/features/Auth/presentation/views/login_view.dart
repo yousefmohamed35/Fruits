bebc1d7e44bb8a32981c29416606655f41eac574
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-09-12 21:42:49 +0300

    Refactor LoginView and LoginViewBody to include form fields for email and password

--- lib/features/Auth/presentation/views/login_view.dart
+++ lib/features/Auth/presentation/views/login_view.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 import '../../../../core/widgets/build_app_bar.dart';
+import 'widgets/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: buildAppBar(context, title: 'تسجيل الدخول'));
+    return Scaffold(
+      appBar: buildAppBar(context, title: 'تسجيل الدخول'),
+      body: const LoginViewBody(),
+    );
   }
 }
--- lib/features/Auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/Auth/presentation/views/widgets/login_view_body.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:fruiteapp/core/widgets/custom_text_form_field.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(child: Column());
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            CustomTextFormField(
+              hintText: 'البريد الإلكتروني',
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'كلمة المرور',
+              keyboardType: TextInputType.visiblePassword,
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.remove_red_eye,
+                  color: Color(0xffC9CECF),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
