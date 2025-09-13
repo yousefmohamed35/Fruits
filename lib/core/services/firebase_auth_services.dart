55b2bb80ebb9ffebe4383f3f392af58184e66e1d
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-09-13 14:40:00 +0300

    Implement user registration with Firebase authentication and custom error handling

--- lib/core/services/firebase_auth_services.dart
+++ lib/core/services/firebase_auth_services.dart
@@ -1,3 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruiteapp/core/error/exception/custom_exception.dart';
+
 class FirebaseAuthServices {
-  // Implement Firebase authentication methods here
+  Future<User> createUserWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: email,
+            password: password,
+          );
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+       throw CustomException('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException('The account already exists for that email.');
+        } else {
+        throw CustomException(e.message ?? 'An unknown error occurred.');
+      }
+    } catch (e) {
+      throw CustomException(e.toString());
+    }
+  }
 }
