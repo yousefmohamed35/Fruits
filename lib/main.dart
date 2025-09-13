9587c5f478119ba977a1bfd4627933d0be4c57cd
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-09-13 14:09:04 +0300

    configure with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'core/services/setup_service_locator.dart';
 import 'core/services/shared_prefrences_services.dart';
 import 'fruite_hub.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setupServiceLocator();
- await getIt<SharedPreferencesService>().init();
+  await getIt<SharedPreferencesService>().init();
   runApp(const FruiteHub());
 }
-
