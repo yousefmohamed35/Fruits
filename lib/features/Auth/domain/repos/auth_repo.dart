f06056be7e5a2b9eb00e9361a5595c4d65cfaa94
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-09-13 14:25:01 +0300

    Refactor AuthRepo to return Either<Failure, UserEntity> for user creation and add dartz dependency

--- lib/features/Auth/domain/repos/auth_repo.dart
+++ lib/features/Auth/domain/repos/auth_repo.dart
@@ -1,6 +1,12 @@
+import 'package:dartz/dartz.dart';
 import 'package:fruiteapp/features/Auth/domain/entities/user_entity.dart';
 
+import '../../../../core/error/failure.dart';
+
 abstract class AuthRepo {
-  Future<UserEntity> createUserWithEmailAndPassword(
-      {required String name, required String email, required String password});
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
 }
