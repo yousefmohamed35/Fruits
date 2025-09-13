b81c7db5272c9eef82af230bbe34a698a5069e5d
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-09-13 13:27:50 +0300

    Implement Custom check box

--- lib/features/Auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/Auth/presentation/views/widgets/signup_view_body.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../core/widgets/custom_text_form_field.dart';
+
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: const [
-          
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            SizedBox(height: 24),
+            CustomTextFormField(
+              hintText: 'الاسم كامل',
+              keyboardType: TextInputType.name,
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'البريد الإلكتروني',
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'كلمة المرور',
+              keyboardType: TextInputType.visiblePassword,
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.remove_red_eye,
+                  color: Color(0xffC9CECF),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
