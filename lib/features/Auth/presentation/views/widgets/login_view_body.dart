6339634d2e3878840045d1e819b0fd3624a8d4ea
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-09-13 12:40:14 +0300

    Add SocialButton widget to LoginViewBody for social login options and finish login ui

--- lib/features/Auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/Auth/presentation/views/widgets/login_view_body.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fruiteapp/core/utils/app_colors.dart';
+import 'package:fruiteapp/core/utils/app_images.dart';
 import 'package:fruiteapp/core/utils/app_text_styles.dart';
 import 'package:fruiteapp/core/widgets/custom_text_form_field.dart';
 
 import '../../../../../core/widgets/custom_button.dart';
 import 'dont_have_account.dart';
 import 'or_divider.dart';
+import 'social_button.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
@@ -17,6 +19,7 @@ class LoginViewBody extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
         child: Column(
           children: [
+            const SizedBox(height: 24),
             CustomTextFormField(
               hintText: 'البريد الإلكتروني',
               keyboardType: TextInputType.emailAddress,
@@ -51,6 +54,24 @@ class LoginViewBody extends StatelessWidget {
             const DontHaveAccount(),
             const SizedBox(height: 32),
             const OrDivider(),
+            const SizedBox(height: 16),
+            SocialButton(
+              title: 'تسجيل بواسطة جوجل',
+              onPressed: () {},
+              imageIcon: Assets.imagesGoogleIcon,
+            ),
+            const SizedBox(height: 16),
+            SocialButton(
+              title: 'تسجيل بواسطة أبل',
+              onPressed: () {},
+              imageIcon: Assets.imagesApplIcon,
+            ),
+            const SizedBox(height: 16),
+            SocialButton(
+              title: 'تسجيل بواسطة فيسبوك',
+              onPressed: () {},
+              imageIcon: Assets.imagesFacebookIcon,
+            ),
           ],
         ),
       ),
